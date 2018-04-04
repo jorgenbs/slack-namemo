89e0dd3b3ca6799d6994cd31f1fe9e7c37cbb0b9
Jørgen Bøe Svendsen <jorgenbs@gmail.com>
2018-04-05 00:38:21 +0200

    sorta mobile availability

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
 /* global API_KEY */
 
 import React from 'react';
+import Router from 'next/router';
+import Link from 'next/link';
 import 'isomorphic-fetch';
 import 'now-env';
-import Link from 'next/link';
 
 function shuffle(array) {
   var currentIndex = array.length,
@@ -35,16 +36,23 @@ const KEY_UP = 38;
 export default class HomePage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { page: 0, reveal: false };
+    this.state = {
+      page: 0,
+      reveal: false
+    };
   }
+
   componentDidMount() {
     document.addEventListener('keydown', this.handleKeyDown.bind(this));
   }
+
   handleKeyDown({ keyCode: key }) {
     if (key === KEY_RIGHT) this.next();
     else if (key === KEY_UP) this.setState({ reveal: true });
   }
-  static async getInitialProps() {
+
+  static async getInitialProps({ req }) {
+    console.log('req', req);
     const channelRes = await fetch(
       `https://slack.com/api/channels.info?token=${API_KEY}&channel=${CHANNEL_ID}`
     );
@@ -54,17 +62,30 @@ export default class HomePage extends React.Component {
     const { members } = await memberRes.json();
     const { channel } = await channelRes.json();
 
-    let filteredMembers = members
-      .filter(m => channel.members.indexOf(m.id) >= 0)
-      .map(member => {
-        return {
-          image: member.profile.image_original || member.profile.image_1024 || member.profile.image_512,
-          name: member.profile.real_name_normalized
-        };
-      });
-    filteredMembers = shuffle(filteredMembers);
-    return { members: filteredMembers };
+    if (members) {
+      let filteredMembers = members
+        .filter(m => channel.members.indexOf(m.id) >= 0)
+        .map(member => {
+          return {
+            image:
+              member.profile.image_original ||
+              member.profile.image_1024 ||
+              member.profile.image_512,
+            name: member.profile.real_name_normalized
+          };
+        });
+      filteredMembers = shuffle(filteredMembers);
+      return { members: filteredMembers };
+    } else {
+      return { members: [] };
+    }
   }
+
+  // componentWillReceiveProps(nextProps) {
+  //   const { pathname, query } = nextProps.url;
+  //   console.log('yo', pathname, query);
+  //   // fetch data based on the new query
+  // }
 
   next() {
     this.setState(prev => ({
@@ -87,6 +108,7 @@ export default class HomePage extends React.Component {
         <div>
           {this.props.members.map((m, i) => (
             <img
+              key={i}
               style={{
                 maxWidth: '400px',
                 display: i === this.state.page ? 'block' : 'none'
@@ -102,13 +124,27 @@ export default class HomePage extends React.Component {
         </div>
         <div>
           <p>
-            Press <bold>UP</bold> to reveal. <bold>RIGHT</bold> to go next
+            Press{' '}
+            <a href="#" onClick={() => this.setState({ reveal: true })}>
+              UP
+            </a>{' '}
+            to reveal.{' '}
+            <a href="#" onClick={() => this.next()}>
+              RIGHT
+            </a>{' '}
+            to go next
           </p>
         </div>
         <div>
           <p>
             {this.state.page + 1}/{this.props.members.length}
           </p>
+        </div>
+        <br />
+        <div>
+          <a href="https://github.com/jorgenbs/slack-flash">
+            make a Pull Request to my ugly code @github
+          </a>
         </div>
       </div>
     );
